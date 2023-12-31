dd90fbd82fac19c46703f3e0339eea0a10a8ec4f
Carson Ball <carsonjball@gmail.com>
2023-12-30 20:09:37 -0500

    prop maker v1.0.0
    finished the prop maker. not perfect but will do for now

--- prop_maker.py
+++ prop_maker.py
@@ -7,6 +7,7 @@ sys.path.append('map maker')
 
 from FacillimumLibrary import Facillimum_Library
 from image_library import ImageLibrary
+from props import Props
 
 
 
@@ -14,7 +15,7 @@ class PropMaker:
     def __init__(self) -> None:
         pygame.init()
 
-        self.screen = pygame.display.set_mode((500,300), pygame.RESIZABLE)
+        self.screen = pygame.display.set_mode((600,400), pygame.RESIZABLE)
 
         pygame.display.set_caption("Aenigma Prop Maker")
 
@@ -24,7 +25,31 @@ class PropMaker:
         # get images
         self.image_library = ImageLibrary()
 
+        self.props = Props(self, "")
+
         pygame.display.set_icon(self.image_library.load_image(self.image_library.SYSTEM_IMAGES[9])[0])
+
+
+##############################
+        self.prop_num = 17
+##############################
+
+
+
+        self.img = self.props.PROP_LIST[self.prop_num][2][0]
+        self.rect = self.props.PROP_LIST[self.prop_num][2][1]
+        self.rect.x = 50
+        self.rect.y = 70
+
+        self.shd_x = self.props.PROP_LIST[self.prop_num][10][0]
+        self.shd_y = self.props.PROP_LIST[self.prop_num][10][1]
+
+        self.shd = pygame.transform.scale(self.props.PROP_LIST[self.prop_num][3][0], (60,30))
+        self.shd_rect = self.shd.get_rect()
+        self.shd_rect.x = self.rect.x + (self.shd_x / 39)
+        self.shd_rect.y = self.rect.y + (self.shd_y / 39)
+
+        # 39
 
 
 
@@ -34,9 +59,28 @@ class PropMaker:
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_x:
+                    self.shd_x = self.FL.open_text_box(30, (15, 10), "black")
+                    self.shd_x = int(self.shd_x) # type: ignore
+                    self.shd_rect.x = self.rect.x + (self.shd_x / 39)
+                if event.key == pygame.K_y:
+                    self.shd_y = self.FL.open_text_box(30, (135, 10), "black")
+                    self.shd_y = int(self.shd_y) # type: ignore
+                    self.shd_rect.y = self.rect.y + (self.shd_y / 39)
+
 
     def draw_all(self):
         self.screen.fill((50,50,50))
+        
+        self.screen.blit(self.shd, self.shd_rect)
+        self.screen.blit(self.img, self.rect)
+
+        self.FL.draw_rect((pygame.Surface.get_size(self.screen)[0], 50), (0,0), (100,100,100))
+
+        self.FL.draw_words("X: "+str(self.shd_x), 30, (10, 10), False, "black")
+        self.FL.draw_words("Y: "+str(self.shd_y), 30, (130, 10), False, "black")
+        
 
 
 
